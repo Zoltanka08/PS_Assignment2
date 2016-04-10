492f45221b129ba5b58acdec26ecec7040ee1914
Zoltan <czako_zozo@yahoo.com>
2016-04-10 19:25:17 +0300

    Create custom exception for Report

---
+++ Services/CustomException/ReportException.cs
@@ -0,0 +1,27 @@
+﻿using Services.CustomExceptions;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.CustomException
+{
+    public class ReportException : Exception
+    {
+        public ReportException()
+        {
+        }
+
+        public ReportException(string message)
+            : base(message)
+        {
+        }
+
+        public ReportException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
