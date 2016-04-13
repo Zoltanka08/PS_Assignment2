937eeafa2185af0663dab3a36a611642a58bc6cf
Zoltan <czako_zozo@yahoo.com>
2016-04-14 00:32:50 +0300

    Add data anotation, password and email regex

--- Bookstore/Models/BookViewModel.cs
+++ Bookstore/Models/BookViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,15 +8,34 @@ namespace Bookstore.Models
 {
     public class BookViewModel
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string Genre { get; set; }
+
+        [Required]
         public string Description { get; set; }
+
+        [Required]
         public string Author { get; set; }
+
+        [Required]
         public int Pages { get; set; }
+
+        [Required]
         public string Published { get; set; }
+
+        [Required]
         public double Price { get; set; }
+
+        [Required]
         public int Quantity { get; set; }
+
+        [Required]
         public int Number { get; set; }
     }
 }
--- Bookstore/Models/UserViewModel.cs
+++ Bookstore/Models/UserViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,12 +9,31 @@ namespace Bookstore.Models
     public class UserViewModel
     {
         public int Id { get; set; }
+
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [RegularExpression(@"^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!*@#$%^&+=_\.]).*$",
+        ErrorMessage = "Password Not Strong Enough Message")]
         public string Password { get; set; }
+
+        [Required]
         public string Firstname { get; set; }
+
+        [Required]
         public string Lastname { get; set; }
+
+        [Required]
         public string Mobile { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Wrong format!")]
         public string Mail { get; set; }
+
+        [Required]
         public string Role { get; set; }
     }
 }
